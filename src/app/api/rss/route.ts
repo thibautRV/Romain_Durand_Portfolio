d4803dbd1d3508daa9450257c2314b1575938952
Thibaut Hervet <thibaut.hervet@epita.fr>
2025-09-28 15:38:58 +0200

    blog routes deleted to build

--- src/app/api/rss/route.ts
+++ src/app/api/rss/route.ts
@@ -1,5 +1,5 @@
 import { getPosts } from "@/utils/utils";
-import { baseURL, blog, person } from "@/resources";
+import { baseURL, person } from "@/resources";
 import { NextResponse } from "next/server";
 
 export async function GET() {
@@ -14,19 +14,12 @@ export async function GET() {
   const rssXml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
-    <title>${blog.title}</title>
-    <link>${baseURL}/blog</link>
-    <description>${blog.description}</description>
     <language>en</language>
     <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
     <atom:link href="${baseURL}/api/rss" rel="self" type="application/rss+xml" />
     <managingEditor>${person.email || "noreply@example.com"} (${person.name})</managingEditor>
     <webMaster>${person.email || "noreply@example.com"} (${person.name})</webMaster>
-    <image>
-      <url>${baseURL}${person.avatar || "/images/avatar.jpg"}</url>
-      <title>${blog.title}</title>
-      <link>${baseURL}/blog</link>
-    </image>
+  
     ${sortedPosts
       .map(
         (post) => `
